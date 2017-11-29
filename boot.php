e375224dfec3706b32bc840337f0b6f546708ace
Mario Vavti <mario@mariovavti.com>
2017-11-29 08:40:18 +0100

    bump version to prevent caching issues related to recent updates to main.js

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/hubloc.php');
 require_once('include/attach.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '2.9.2' );
+define ( 'STD_VERSION',             '2.9.3' );
 define ( 'ZOT_REVISION',            '1.3' );
 
 define ( 'DB_UPDATE_VERSION',       1198  );
