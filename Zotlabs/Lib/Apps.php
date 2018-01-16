03f692f24fe543c740e5443223d8084b9ca0c4de
zotlabs <mike@macgirvin.com>
2018-01-15 21:46:27 -0800

    surface the article feature

--- Zotlabs/Lib/Apps.php
+++ Zotlabs/Lib/Apps.php
@@ -221,6 +221,7 @@ class Apps {
 	static public function translate_system_apps(&$arr) {
 		$apps = array(
 			'Apps' => t('Apps'),
+			'Articles' => t('Articles'),
 			'Cards' => t('Cards'),
 			'Admin' => t('Site Admin'),
 			'Report Bug' => t('Report Bug'),
