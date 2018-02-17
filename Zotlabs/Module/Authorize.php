4a5d1076eda66f4d562219468b84dff2bd8de86b
Klaus Weidenbach <Klaus.Weidenbach@gmx.net>
2018-02-17 01:49:01 +0100

    Refactor OAuth2Server a bit.

--- Zotlabs/Module/Authorize.php
+++ Zotlabs/Module/Authorize.php
@@ -2,12 +2,12 @@
 
 namespace Zotlabs\Module;
 
+use Zotlabs\Identity\OAuth2Storage;
+
 
 class Authorize extends \Zotlabs\Web\Controller {
 
-
 	function init() {
-
 
 		// workaround for HTTP-auth in CGI mode
 		if (x($_SERVER, 'REDIRECT_REMOTE_USER')) {
@@ -28,41 +28,40 @@ class Authorize extends \Zotlabs\Web\Controller {
 			}
 		}
 
-		$s = new \Zotlabs\Identity\OAuth2Server();
+		$s = new \Zotlabs\Identity\OAuth2Server(new OAuth2Storage(\DBA::$dba->db));
 
 		$request = \OAuth2\Request::createFromGlobals();
 		$response = new \OAuth2\Response();
 
 		// validate the authorize request
-		if (! $s->server->validateAuthorizeRequest($request, $response)) {
-		    $response->send();
-    		killme();
+		if (! $s->validateAuthorizeRequest($request, $response)) {
+			$response->send();
+			killme();
 		}
 
-	// display an authorization form
-	if (empty($_POST)) {
+		// display an authorization form
+		if (empty($_POST)) {
 
-	  return '
+			return '
 <form method="post">
   <label>Do You Authorize TestClient?</label><br />
   <input type="submit" name="authorized" value="yes">
   <input type="submit" name="authorized" value="no">
 </form>';
-	}
+		}
 
-	// print the authorization code if the user has authorized your client
-	$is_authorized = ($_POST['authorized'] === 'yes');
-	$s->server->handleAuthorizeRequest($request, $response, $is_authorized, local_channel());
-	if ($is_authorized) {
-		// this is only here so that you get to see your code in the cURL request. Otherwise, 
-		// we'd redirect back to the client
-		$code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=')+5, 40);
-		echo("SUCCESS! Authorization Code: $code");
-		
-	}
+		// print the authorization code if the user has authorized your client
+		$is_authorized = ($_POST['authorized'] === 'yes');
+		$s->handleAuthorizeRequest($request, $response, $is_authorized, local_channel());
+		if ($is_authorized) {
+			// this is only here so that you get to see your code in the cURL request. Otherwise,
+			// we'd redirect back to the client
+			$code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=')+5, 40);
+			echo("SUCCESS! Authorization Code: $code");
+		}
 
-	$response->send();
-	killme();
+		$response->send();
+		killme();
 	}
 
 }
