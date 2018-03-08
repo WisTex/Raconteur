2105cfd433d30e0277e5ae3080cec8620747759a
Mario <mario@mariovavti.com>
2018-03-08 10:20:40 +0100

    fix syntax error in postgres update

--- Zotlabs/Update/_1204.php
+++ Zotlabs/Update/_1204.php
@@ -8,11 +8,11 @@ class _1204 {
 
 		if(ACTIVE_DBTYPE == DBTYPE_POSTGRES) {
 			$r1 = q("ALTER TABLE poll ADD poll_guid text NOT NULL");
-			$r2 = q("create index \"poll_guid_idx\" on poll \"poll_guid\"");
+			$r2 = q("create index \"poll_guid_idx\" on poll (\"poll_guid\")");
 			$r3 = q("ALTER TABLE poll_elm ADD pelm_guid text NOT NULL");
-			$r4 = q("create index \"pelm_guid_idx\" on poll_elm \"pelm_guid\"");
+			$r4 = q("create index \"pelm_guid_idx\" on poll_elm (\"pelm_guid\")");
 			$r5 = q("ALTER TABLE vote ADD vote_guid text NOT NULL");
-			$r6 = q("create index \"vote_guid_idx\" on vote \"vote_guid\"");
+			$r6 = q("create index \"vote_guid_idx\" on vote (\"vote_guid\")");
 
 			$r = ($r1 && $r2 && $r3 && $r4 && $r5 && $r6);
 		}
