19782655e6110c615b017059ae5b3e10e7d71d60
Mario Vavti <mario@mariovavti.com>
2018-04-18 08:39:43 +0200

    version bump and strings

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '3.3.5' );
+define ( 'STD_VERSION',             '3.4RC' );
 define ( 'ZOT_REVISION',            '6.0a' );
 
 
