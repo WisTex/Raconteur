16dcbc1e674cef1d51dc0e81f91d7055350f9990
Mario Vavti <mario@mariovavti.com>
2018-04-18 08:52:10 +0200

    bump version

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '3.4RC' );
+define ( 'STD_VERSION',             '3.5' );
 define ( 'ZOT_REVISION',            '6.0a' );
 
 
