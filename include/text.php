f923d21df341546e054eb0f9628891365c4c2d62
zotlabs <mike@macgirvin.com>
2018-04-25 18:41:19 -0700

    some tagging work

--- include/text.php
+++ include/text.php
@@ -3402,3 +3402,18 @@ function unpunify($s) {
 
 }
 
+
+function unique_multidim_array($array, $key) {
+    $temp_array = array();
+    $i = 0;
+    $key_array = array();
+   
+    foreach($array as $val) {
+        if (!in_array($val[$key], $key_array)) {
+            $key_array[$i] = $val[$key];
+            $temp_array[$i] = $val;
+        }
+        $i++;
+    }
+    return $temp_array;
+} 
