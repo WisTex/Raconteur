2dd66df8ebb395a7154968f14c77390ec75460fb
Galette Saucisse <philip@wittamore.com>
2018-05-18 16:05:30 +0200

    Update hstrings.php

--- view/fr/hstrings.php
+++ view/fr/hstrings.php
@@ -1,7 +1,7 @@
 <?php
 
-if(! function_exists("string_plural_select_fr.")) {
-function string_plural_select_fr.($n){
+if(! function_exists("string_plural_select_fr")) {
+function string_plural_select_fr($n){
 	return ($n > 1);;
 }}
 App::$rtl = 0;
