9ce94bd414441e1a7d922148603e474ed9a19fb2
zotlabs <mike@macgirvin.com>
2018-08-22 17:53:17 -0700

    product name

--- boot.php
+++ boot.php
@@ -31,7 +31,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 require_once('include/items.php');
 
-define ( 'PLATFORM_NAME',           'zap' );
+define ( 'PLATFORM_NAME',           'osada' );
 define ( 'STD_VERSION',             '0.0.1' );
 define ( 'ZOT_REVISION',            '6.0' );
 
