00eeb680d9146760cd6df39f6b5fc5e9cac71413
zotlabs <mike@macgirvin.com>
2018-09-06 18:33:27 -0700

    set a process timeout for fetching remote xchan photos

--- Zotlabs/Daemon/Xchan_photo.php
+++ Zotlabs/Daemon/Xchan_photo.php
@@ -12,6 +12,11 @@ class Xchan_photo {
 		$url = hex2bin($argv[1]);
 		$xchan = hex2bin($argv[2]);
 
+		logger('daemon_xchan_photo start');
+
+		// Some photo sources hang after connect and aren't caught by curl timeout
+
+		set_time_limit(60);
 
 		$photos = import_xchan_photo($url,$xchan);
 		$r = q("update xchan set xchan_photo_date = '%s', xchan_photo_l = '%s', xchan_photo_m = '%s', xchan_photo_s = '%s', xchan_photo_mimetype = '%s' where xchan_hash = '%s'",
@@ -22,6 +27,8 @@ class Xchan_photo {
             dbesc($photos[3]),
             dbesc($xchan)
         );
+
+		logger('daemon_xchan_photo finish');
 
 		return;
 	}
