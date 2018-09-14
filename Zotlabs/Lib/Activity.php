767d1f98898abc3921fa26a84cad3ca200d24c4a
zotlabs <mike@macgirvin.com>
2018-09-13 18:42:29 -0700

    missing call to actor_store

--- Zotlabs/Lib/Activity.php
+++ Zotlabs/Lib/Activity.php
@@ -1391,6 +1391,8 @@ class Activity {
 		$s['owner_xchan']  = $act->actor['id'];
 		$s['author_xchan'] = $act->actor['id'];
 
+		self::actor_store($act->actor['id'],$act->actor);
+
 		$s['mid']        = $act->obj['id'];
 		$s['parent_mid'] = $act->parent_id;
 
