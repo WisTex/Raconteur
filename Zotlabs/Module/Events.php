382551e4b4063be77f987df7be376b47c6d82ef5
zotlabs <mike@macgirvin.com>
2018-09-20 22:50:12 -0700

    strip bbcode from event titles

--- Zotlabs/Module/Events.php
+++ Zotlabs/Module/Events.php
@@ -7,7 +7,7 @@ require_once('include/conversation.php');
 require_once('include/bbcode.php');
 require_once('include/datetime.php');
 require_once('include/event.php');
-
+require_once('include/html2plain.php');
 
 class Events extends \Zotlabs\Web\Controller {
 
@@ -642,6 +642,7 @@ class Events extends \Zotlabs\Web\Controller {
 					}
 					$html = format_event_html($rr);
 					$rr['desc'] = zidify_links(smilies(bbcode($rr['desc'])));
+					$rr['description'] = htmlentities(html2plain(bbcode($rr['description'])),ENT_COMPAT,'UTF-8',false);
 					$rr['location'] = zidify_links(smilies(bbcode($rr['location'])));
 					$events[] = array(
 						'id'=>$rr['id'],
