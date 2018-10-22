20f7479b83da8d3735a32ffd2d2e19cb4087bf6a
zotlabs <mike@macgirvin.com>
2018-10-21 18:07:19 -0700

    roll minor release number

--- boot.php
+++ boot.php
@@ -32,7 +32,7 @@ require_once('include/bbcode.php');
 require_once('include/items.php');
 
 
-define ( 'STD_VERSION',             '1.4.2' );
+define ( 'STD_VERSION',             '1.4.3' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1221 );
