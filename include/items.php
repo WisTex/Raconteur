bb4cbbba0bf0c6fd40535e3df722d12d5d7e5fd0
DM42.Net (Matt Dent) <dentm42@dm42.net>
2018-11-03 22:28:24 -0400

    Fix: Uploaded photo perms set to 'only me' even if force_public_uploads = 1

--- include/items.php
+++ include/items.php
@@ -4617,7 +4617,7 @@ function fix_attached_photo_permissions($uid,$xchan_hash,$body,
 					continue;
 				$image_uri = substr($image,strrpos($image,'/') + 1);
 				if(strpos($image_uri,'-') !== false)
-					$image_uri = substr($image_uri,0, strpos($image_uri,'-'));
+					$image_uri = substr($image_uri,0, strrpos($image_uri,'-'));
 				if(strpos($image_uri,'.') !== false)
 					$image_uri = substr($image_uri,0, strpos($image_uri,'.'));
 				if(! strlen($image_uri))
