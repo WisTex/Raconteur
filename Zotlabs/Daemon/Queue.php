366f7c3fe663ced47a77870ec32de72c2461c06f
M. Dent <dentm42@gmail.com>
2019-01-11 14:13:56 +0100

    use db_getfunc().

--- Zotlabs/Daemon/Queue.php
+++ Zotlabs/Daemon/Queue.php
@@ -9,16 +9,6 @@ class Queue {
 
 	static public function run($argc,$argv) {
 
-                switch (DBTYPE_ACTIVE) {
-                        case DBTYPE_MYSQL:
-				$sqlrandfunc = "RAND()";
-                                break;
-
-                        case DBTYPE_POSTGRESQL:
-				$sqlrandfunc = "RANDOM()";
-                                break;
-                }
-
 		if($argc > 1)
 			$queue_id = $argv[1];
 		else
@@ -68,6 +58,8 @@ class Queue {
 			// the site is permanently down, there's no reason to attempt delivery at all, or at most not more than once 
 			// or twice a day. 
 
+            $sqlrandfunc = db_getfunc('rand');
+            
 			$r = q("SELECT *,$sqlrandfunc as rn FROM outq WHERE outq_delivered = 0 and outq_scheduled < %s order by rn limit 1",
 				db_utcnow()
 			);
