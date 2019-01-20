35f1cee50b6f2ace3c3acb948a8b41578f259688
zotlabs <mike@macgirvin.com>
2019-01-20 15:52:02 -0800

    more comment policy work

--- Zotlabs/Lib/Libzot.php
+++ Zotlabs/Lib/Libzot.php
@@ -1178,7 +1178,7 @@ class Libzot {
 
 				logger($AS->debug());
 
-				$r = q("select hubloc_hash from hubloc where hubloc_id_url = '%s' and hubloc_network = 'zot6' limit 1",
+				$r = q("select hubloc_hash, hubloc_url from hubloc where hubloc_id_url = '%s' and hubloc_network = 'zot6' limit 1",
 					dbesc($AS->actor['id'])
 				); 
 
@@ -1187,7 +1187,7 @@ class Libzot {
 				}
 
 
-				$s = q("select hubloc_hash from hubloc where hubloc_id_url = '%s' and hubloc_network = 'zot6' limit 1",
+				$s = q("select hubloc_hash, hubloc_url from hubloc where hubloc_id_url = '%s' and hubloc_network = 'zot6' limit 1",
 					dbesc($env['sender'])
 				); 
 
@@ -1208,10 +1208,10 @@ class Libzot {
 
 					// set comment policy depending on source hub. Unknown or osada is ActivityPub.
 					// Anything else we'll say is zot - which could have a range of project names
-
 					$s = q("select site_project from site where site_url = '%s' limit 1",
-						dbesc($hubloc['hubloc_url'])
+						dbesc($r[0]['hubloc_url'])
 					);
+
 					if((! $s) || (in_array($s[0]['site_project'],[ '', 'osada' ]))) {
 						$arr['comment_policy'] = 'authenticated';
 					}
