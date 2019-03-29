5bcf3928161aa296e1cce683ada6bd3d0b10c3c9
zotlabs <mike@macgirvin.com>
2019-03-29 13:27:23 -0700

    add hierarchy to channels for collection support and other purposes

--- boot.php
+++ boot.php
@@ -35,7 +35,7 @@ require_once('include/items.php');
 define ( 'STD_VERSION',             '2.9' );
 define ( 'ZOT_REVISION',            '6.0' );
 
-define ( 'DB_UPDATE_VERSION',       1230 );
+define ( 'DB_UPDATE_VERSION',       1231 );
 
 define ( 'PROJECT_BASE',   __DIR__ );
 
