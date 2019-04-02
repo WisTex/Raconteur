7093c2d5140dbc2e7e5e48f1568fa59f9ed91672
zotlabs <mike@macgirvin.com>
2019-04-01 19:04:08 -0700

    use affinity slider to set default affinity slider values

--- Zotlabs/Module/Affinity.php
+++ Zotlabs/Module/Affinity.php
@@ -20,7 +20,7 @@ class Affinity extends \Zotlabs\Web\Controller {
 			$cmin = intval($_POST['affinity_cmin']);
 			if($cmin < 0 || $cmin > 99)
 				$cmin = 0;
-			set_pconfig(local_channel(),'affinity','cmin',$cmin);
+			set_pconfig(local_channel(),'affinity','cmin',0);
 			set_pconfig(local_channel(),'affinity','cmax',$cmax);
 
 			info( t('Affinity Tool settings updated.') . EOL);
@@ -42,20 +42,42 @@ class Affinity extends \Zotlabs\Web\Controller {
             return $text;
         }
 
-		$text .= EOL . t('The numbers below represent the minimum and maximum slider default positions for your network/stream page as a percentage.') . EOL . EOL; 			
+		$text .= EOL . t('The number below represents the default maximum slider position for your network/stream page as a percentage.') . EOL . EOL; 			
 
 		$setting_fields = $text;
 
 		$cmax = intval(get_pconfig(local_channel(),'affinity','cmax'));
 		$cmax = (($cmax) ? $cmax : 99);
-		$setting_fields .= replace_macros(get_markup_template('field_input.tpl'), array(
-			'$field'    => array('affinity_cmax', t('Default maximum affinity level'), $cmax, t('0-99 default 99'))
-		));
-		$cmin = intval(get_pconfig(local_channel(),'affinity','cmin'));
-		$cmin = (($cmin) ? $cmin : 0);
-		$setting_fields .= replace_macros(get_markup_template('field_input.tpl'), array(
-			'$field'    => array('affinity_cmin', t('Default minimum affinity level'), $cmin, t('0-99 - default 0'))
-		));
+//		$setting_fields .= replace_macros(get_markup_template('field_input.tpl'), array(
+//			'$field'    => array('affinity_cmax', t('Default maximum affinity level'), $cmax, t('0-99 default 99'))
+//		));
+
+		if(Apps::system_app_installed(local_channel(),'Affinity Tool')) {
+
+			$labels = array(
+				0  => t('Me'),
+				20 => t('Family'),
+				40 => t('Friends'),
+				60 => t('Peers'),
+				80 => t('Connections'),
+				99 => t('All')
+			);
+			call_hooks('affinity_labels',$labels);
+
+			$tpl = get_markup_template('affinity.tpl');
+			$x = replace_macros($tpl, [
+				'$cmin' => 0,
+				'$cmax' => $cmax,
+				'$lbl'  => t('Default friend zoom in/out'),
+				'$refresh' => t('Refresh'),
+				'$labels' => $labels,
+			]);
+		
+
+			$arr = array('html' => $x);
+			call_hooks('affinity_slider',$arr);
+			$setting_fields .=  $arr['html'];
+		}
 
 		$s .= replace_macros(get_markup_template('generic_app_settings.tpl'), array(
 			'$addon'    => array('affinity', '' . t('Affinity Tool Settings'), '', t('Submit')),
