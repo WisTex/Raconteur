483e41e420558d50f899db9388439d300baabd00
zotlabs <mike@macgirvin.com>
2019-06-24 17:53:47 -0700

    rev update

--- boot.php
+++ boot.php
@@ -45,7 +45,7 @@ require_once('include/items.php');
 
 
 
-define ( 'STD_VERSION',             '3.1' );
+define ( 'STD_VERSION',             '3.2' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1232 );
