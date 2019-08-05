045a07c6dc163852f10c407e7fa846680e966ca6
zotlabs <mike@macgirvin.com>
2019-08-04 23:49:08 -0700

    feed author issues

--- include/items.php
+++ include/items.php
@@ -956,8 +956,8 @@ function import_author_rss($x) {
 
 	$r = xchan_store_lowlevel(
 		[
-			'xchan_hash'         => $x['guid'],
-			'xchan_guid'         => $x['guid'],
+			'xchan_hash'         => $x['url'],
+			'xchan_guid'         => $x['url'],
 			'xchan_url'          => $x['url'],
 			'xchan_name'         => (($name) ? $name : t('(Unknown)')),
 			'xchan_name_date'    => datetime_convert(),
