6fe52e8a076ff6c435df170eb20552d8882cb625
zotlabs <mike@macgirvin.com>
2019-11-12 21:04:24 -0800

    rev update

--- boot.php
+++ boot.php
@@ -48,7 +48,7 @@ require_once('include/items.php');
 
 
 
-define ( 'STD_VERSION',             '19.11.11' );
+define ( 'STD_VERSION',             '19.11.13' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1236 );
