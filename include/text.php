1a181fd543bb45f70160ee6088a3ab8f0658bfdd
zotlabs <mike@macgirvin.com>
2020-07-07 17:54:08 -0700

    second attempt to handle Pleroma's "source" field correctly in the cases where it has no mediatype and content elements

--- include/text.php
+++ include/text.php
@@ -3559,7 +3559,7 @@ function array_path_exists($str,$arr) {
 
 	if ($search) {
 		foreach ($search as $s) {
-			if ($ptr && array_key_exists($s,$ptr)) {
+			if ($ptr && is_array($ptr) && array_key_exists($s,$ptr)) {
 				$ptr = $ptr[$s];
 			}
 			else {
