4dc4572f949b2f3f220cd5ae47f6d1df2e84e02b
nobody <nobody@zotlabs.com>
2020-08-07 23:06:01 -0700

    use channel_hash for lookup

--- Zotlabs/Module/Channel.php
+++ Zotlabs/Module/Channel.php
@@ -89,7 +89,7 @@ class Channel extends Controller {
 			$sigdata = HTTPSig::verify(file_get_contents('php://input'), EMPTY_STR, 'zot6');
 
 			if($sigdata && $sigdata['signer'] && $sigdata['header_valid']) {
-				$data = json_encode(Libzot::zotinfo([ 'address' => $channel['channel_address'], 'target_url' => $sigdata['signer'] ]));
+				$data = json_encode(Libzot::zotinfo([ 'guid_hash' => $channel['channel_hash'], 'target_url' => $sigdata['signer'] ]));
 				$s = q("select site_crypto, hubloc_sitekey from site left join hubloc on hubloc_url = site_url where hubloc_id_url = '%s' and hubloc_network = 'zot6' limit 1",
 					dbesc($sigdata['signer'])
 				);
@@ -99,7 +99,7 @@ class Channel extends Controller {
 				}
 			}
 			else {
-				$data = json_encode(Libzot::zotinfo([ 'address' => $channel['channel_address'] ]));
+				$data = json_encode(Libzot::zotinfo([ 'guid_hash' => $channel['channel_hash'] ]));
 			}
 
 			$headers = [ 
