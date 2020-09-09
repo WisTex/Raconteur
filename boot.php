1c62f7f5ad40a0e6f62ac7778d8dc5dbfab9e0da
nobody <nobody@zotlabs.com>
2020-09-09 02:21:35 -0700

    revision

--- boot.php
+++ boot.php
@@ -16,7 +16,7 @@ use Zotlabs\Daemon\Run;
  * @brief This file defines some global constants and includes the central App class.
  */
 
-define ( 'STD_VERSION',             '20.09.07' );
+define ( 'STD_VERSION',             '20.09.09' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1242 );
