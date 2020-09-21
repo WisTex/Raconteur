e5bbe51b11147d9585984c91a746deaeda221952
nobody <nobody@zotlabs.com>
2020-09-20 22:26:58 -0700

    cleanup

--- Zotlabs/Module/Filestorage.php
+++ Zotlabs/Module/Filestorage.php
@@ -18,7 +18,7 @@ class Filestorage extends Controller {
 
 		$channel_id = ((x($_POST, 'uid')) ? intval($_POST['uid']) : 0);
 
-		if((! $channel_id) || (! local_channel()) || ($channel_id != local_channel())) {
+		if ((! $channel_id) || (! local_channel()) || ($channel_id != local_channel())) {
 			notice( t('Permission denied.') . EOL);
 			return;
 		}
@@ -27,10 +27,10 @@ class Filestorage extends Controller {
 		$resource = ((x($_POST, 'filehash')) ? notags($_POST['filehash']) : '');
 		$notify = ((x($_POST, 'notify_edit')) ? intval($_POST['notify_edit']) : 0);
 
-		$newname = ((x($_POST, 'newname')) ? notags($_POST['newname']) : '');
+		$newname = ((x($_POST, 'newname')) ? notags(trim($_POST['newname'])) : '');
 		$newdir  = ((x($_POST, 'newdir'))  ? notags($_POST['newdir'])  : false);
 
-		if(! $resource) {
+		if (! $resource) {
 			notice(t('Item not found.') . EOL);
 			return;
 		}
@@ -87,49 +87,45 @@ class Filestorage extends Controller {
 
 	function get() {
 
-		if(argc() > 1)
-			$which = argv(1);
-		else {
-			notice( t('Requested profile is not available.') . EOL );
+		if (argc() > 1) {
+			$channel = channelx_by_nick(argv(1));
+		}
+		if (! $channel) {
+			notice( t('Channel unavailable.') . EOL);
 			App::$error = 404;
 			return;
-		}
+		}			
 
-		$r = q("select * from channel where channel_address = '%s'",
-			dbesc($which)
-		);
-		if($r) {
-			$channel = $r[0];
-			$owner = intval($r[0]['channel_id']);
-		}
-
+		$owner = intval($channel['channel_id']);
 		$observer = App::get_observer();
+		
 		$ob_hash = (($observer) ? $observer['xchan_hash'] : '');
 
 		$perms = get_all_perms($owner, $ob_hash);
 
-		if(! ($perms['view_storage'] || is_site_admin())){
+		if (! ($perms['view_storage'] || is_site_admin())){
 			notice( t('Permission denied.') . EOL);
 			return;
 		}
 
 
-		if(argc() > 3 && argv(3) === 'delete') {
+		if (argc() > 3 && argv(3) === 'delete') {
 
-			if(argc() > 4 && argv(4) === 'json')
+			if (argc() > 4 && argv(4) === 'json') {
 				$json_return = true;
-
+			}
 
 			$admin_delete = false;
 
-			if(! $perms['write_storage']) {
-				if(is_site_admin()) {
+			if (! $perms['write_storage']) {
+				if (is_site_admin()) {
 					$admin_delete = true;
 				}
 				else {
 					notice( t('Permission denied.') . EOL);
-					if($json_return) 
+					if ($json_return) {
 						json_return_and_die([ 'success' => false ]);
+					}
 					return;
 				}
 			}
@@ -139,67 +135,65 @@ class Filestorage extends Controller {
 				dbesc($file),
 				intval($owner)
 			);
-			if(! $r) {
+			if (! $r) {
 				notice( t('File not found.') . EOL);
 
-				if($json_return) 
+				if ($json_return) {
 					json_return_and_die([ 'success' => false ]);
+				}
 
 				goaway(z_root() . '/cloud/' . $which);
 			}
 
-			if(local_channel() !== $owner) {
-				if($r[0]['creator'] && $r[0]['creator'] !== $ob_hash) {
+			$f = array_shift($r);
+
+			if (intval(local_channel()) !== $owner) {
+				if ($f['creator'] && $f['creator'] !== $ob_hash) {
 					notice( t('Permission denied.') . EOL);
 
-					if($json_return) 
+					if ($json_return) {
 						json_return_and_die([ 'success' => false ]);
-
+					}
 					goaway(z_root() . '/cloud/' . $which);
 				}
 			}
-
-
-			$f = $r[0];
-
-			$channel = channelx_by_n($owner);
 
 			$url = get_cloud_url($channel['channel_id'], $channel['channel_address'], $f['hash']);
 
 			attach_delete($owner, $f['hash']);
 
-			if(! $admin_delete) {
+			if (! $admin_delete) {
 				$sync = attach_export_data($channel, $f['hash'], true);
-				if($sync) {
-					Libsync::build_sync_packet($channel['channel_id'], array('file' => array($sync)));
+				if ($sync) {
+					Libsync::build_sync_packet($channel['channel_id'], [ 'file' => [ $sync ] ]);
 				}
 			}
 
-			if($json_return)
+			if ($json_return) {
 				json_return_and_die([ 'success' => true ]);
+			}
 
 			goaway(dirname($url));
 		}
-
-
 
 
 		// Since we have ACL'd files in the wild, but don't have ACL here yet, we
 		// need to return for anyone other than the owner, despite the perms check for now.
 
 		$is_owner = (((local_channel()) && ($owner  == local_channel())) ? true : false);
-		if(! ($is_owner || is_site_admin())){
-			info( t('Permission Denied.') . EOL );
+		if (! ($is_owner || is_site_admin())) {
+			notice( t('Permission denied.') . EOL );
 			return;
 		}
 
 
-		if(argc() > 3 && argv(3) === 'edit') {
+		if (argc() > 3 && argv(3) === 'edit') {
 			require_once('include/acl_selectors.php');
-			if(! $perms['write_storage']) {
+			if (! $perms['write_storage']) {
 				notice( t('Permission denied.') . EOL);
 				return;
 			}
+			
 			$file = intval(argv(2));
 
 			$r = q("select id, uid, folder, filename, revision, flags, is_dir, os_storage, hash, allow_cid, allow_gid, deny_cid, deny_gid from attach where id = %d and uid = %d limit 1",
@@ -207,7 +201,8 @@ class Filestorage extends Controller {
 				intval($owner)
 			);
 
-			$f = $r[0];
+			$f = array_shift($r);
+			
 			$channel = App::get_channel();
 
 			$cloudpath = get_cloudpath($f);
@@ -222,7 +217,7 @@ class Filestorage extends Controller {
 			$encoded_path = str_replace('%2F', '/', rawurlencode($cloudpath));
 			$folder_list = attach_folder_select_list($channel['channel_id']);
 
-			$o = replace_macros(get_markup_template('attach_edit.tpl'), array(
+			$o = replace_macros(get_markup_template('attach_edit.tpl'), [
 				'$header' => t('Edit file permissions'),
 				'$file' => $f,
 				'$cloudpath' => z_root() . '/' . $encoded_path,
@@ -235,10 +230,10 @@ class Filestorage extends Controller {
 				'$deny_cid' => acl2json($f['deny_cid']),
 				'$deny_gid' => acl2json($f['deny_gid']),
 				'$lockstate' => $lockstate,
-				'$newname' => [ 'newname', t('Change filename to') , '', '' ],
+				'$newname' => [ 'newname', t('Change filename to') , '', t('Leave blank to keep the existing filename') ],
 				'$newdir'  => [ 'newdir', t('Move to directory'), $f['folder'], '', $folder_list ],
 				'$permset' => t('Set/edit permissions'),
-				'$recurse' => array('recurse', t('Include all files and sub folders'), 0, '', array(t('No'), t('Yes'))),
+				'$recurse' => [ 'recurse', t('Include all files and sub folders'), 0, '', [ t('No'), t('Yes') ] ],
 				'$backlink' => t('Return to file list'),
 				'$isadir' => $is_a_dir,
 				'$cpdesc' => t('Copy/paste this code to attach file to a post'),
@@ -246,14 +241,12 @@ class Filestorage extends Controller {
 				'$submit' => t('Submit'),
 				'$attach_btn_title' => t('Share this file'),
 				'$link_btn_title' => t('Show URL to this file'),
-				'$notify' => array('notify_edit', t('Show in your contacts shared folder'), 0, '', array(t('No'), t('Yes'))),
-			));
+				'$notify' => [ 'notify_edit', t('Show in your contacts shared folder'), 0, '', [ t('No'), t('Yes') ] ],
+			]);
 
 			echo $o;
 			killme();
 		}
-
 		goaway(z_root() . '/cloud/' . $which);
 	}
-
 }
