3aad607a266d5881f966830a38b081611b1eaf76
nobody <nobody@zotlabs.com>
2020-09-22 18:10:11 -0700

    missing daemon

---
+++ Zotlabs/Daemon/Convo.php
@@ -0,0 +1,53 @@
+<?php /** @file */
+
+namespace Zotlabs\Daemon;
+
+use Zotlabs\Lib\Activity;
+use Zotlabs\Lib\ActivityStreams;
+
+
+class Convo {
+
+	static public function run($argc,$argv) {
+
+		if($argc != 4) {
+			killme();
+		}
+
+		$id = $argv[1];
+		$channel_id = intval($argv[2]);
+		$contact_hash = $argv[3];
+		
+		$channel = channelx_by_n($channel_id);
+		if (! $channel) {
+			killme();
+		}
+	
+		$r = q("SELECT abook.*, xchan.* FROM abook left join xchan on abook_xchan = xchan_hash
+			WHERE abook_channel = %d and abook_xchan = '%s' LIMIT 1",
+			intval($channel_id),
+			intval($contact_hash)
+		);
+		if (! $r) {
+			killme();
+		}
+		
+		$contact = array_shift($r);
+
+		$obj = new ASCollection($id, $channel);
+
+		$messages = $obj->get();
+		if ($messages) {	
+			foreach ($messages as $message) {
+				if (is_string($message)) {
+					$message = Activity::fetch($message,$channel);
+				}
+				$AS = new ActivityStreams($message);
+				if ($AS->is_valid() && is_array($AS->obj)) {
+					$item = Activity::decode_note($AS,true);
+					Activity::store($channel,$contact['abook_xchan'],$AS,$item);
+				}
+			}
+		}
+	}
+}
