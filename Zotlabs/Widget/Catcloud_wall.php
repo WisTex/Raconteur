38db595c127bdc7e099e2e2f1fbe061f5cfc49ab
nobody <nobody@zotlabs.com>
2020-10-21 15:22:23 -0700

    tagadelic app

--- Zotlabs/Widget/Catcloud_wall.php
+++ Zotlabs/Widget/Catcloud_wall.php
@@ -2,18 +2,20 @@
 
 namespace Zotlabs\Widget;
 
+use App;
+
 class Catcloud_wall {
 
 	function widget($arr) {
 
-		if((! \App::$profile['profile_uid']) || (! \App::$profile['channel_hash']))
+		if((! App::$profile['profile_uid']) || (! App::$profile['channel_hash']))
 			return '';
-		if(! perm_is_allowed(\App::$profile['profile_uid'], get_observer_hash(), 'view_stream'))
+		if(! perm_is_allowed(App::$profile['profile_uid'], get_observer_hash(), 'view_stream'))
 			return '';
 
 		$limit = ((array_key_exists('limit',$arr)) ? intval($arr['limit']) : 50);
 
-		return catblock(\App::$profile['profile_uid'], $limit, '', \App::$profile['channel_hash'], 'wall');
+		return catblock(App::$profile['profile_uid'], $limit, '', App::$profile['channel_hash'], 'wall');
 	}
 
 }
