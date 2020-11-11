2d6db55961cdd7af1f82dae3ff7a9d211ff743be
nobody <nobody@zotlabs.com>
2020-11-10 17:11:19 -0800

    federate categories

--- boot.php
+++ boot.php
@@ -468,7 +468,7 @@ define ( 'NAMESPACE_YMEDIA',          'http://search.yahoo.com/mrss/' );
 
 define ( 'ACTIVITYSTREAMS_JSONLD_REV', 'https://www.w3.org/ns/activitystreams' );
 
-define ( 'ZOT_APSCHEMA_REV', '/apschema/v1.18' );
+define ( 'ZOT_APSCHEMA_REV', '/apschema/v1.19' );
 
 /**
  * activity stream defines
