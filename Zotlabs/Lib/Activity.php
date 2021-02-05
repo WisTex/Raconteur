b9f8585341c49088f23db1c435ceb1f1e5e9687b
nobody <nobody@zotlabs.com>
2021-02-04 16:35:18 -0800

    Attempt to force mentions to use the default mention rewrite conventions for the network involved (e.g. username vs. displayname). This will only work for original activities, not relayed activities which need to have been signed by the sender and therefore cannot be modified.

--- Zotlabs/Lib/Activity.php
+++ Zotlabs/Lib/Activity.php
@@ -2738,18 +2738,7 @@ class Activity {
 
 	}
 
-	static function rewrite_mentions(&$s) {
-		// rewrite incoming mentions in accordance with system.tag_username setting
-		// 0 - displayname
-		// 1 - username
-		// 2 - displayname (username)
-		// 127 - default
-		
-		$pref = intval(PConfig::Get($s['uid'],'system','tag_username',Config::Get('system','tag_username',false)));
-
-		if ($pref === 127) {
-			return;
-		}
+	static function rewrite_mentions_sub(&$s, $pref) {
 
 		if ($s['term']) {
 			foreach ($s['term'] as $tag) {
@@ -2809,13 +2798,32 @@ class Activity {
 				}
 			}
 		}
-		
+
 		// $s['html'] will be populated if caching was enabled.
 		// This is usually the case for ActivityPub sourced content, while Zot6 content is not cached.
 
 		if ($s['html']) {
 			$s['html'] = bbcode($s['body'], [ 'bbonly' => true ] );
 		}
+
+		return;
+	}
+
+	static function rewrite_mentions(&$s) {
+		// rewrite incoming mentions in accordance with system.tag_username setting
+		// 0 - displayname
+		// 1 - username
+		// 2 - displayname (username)
+		// 127 - default
+		
+		$pref = intval(PConfig::Get($s['uid'],'system','tag_username',Config::Get('system','tag_username',false)));
+
+		if ($pref === 127) {
+			return;
+		}
+
+		self::rewrite_mentions_sub($s,$pref);
+
 
 		return;
 	}
