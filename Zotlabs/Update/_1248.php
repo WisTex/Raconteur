1b79f1f885585eebcd44ab32187a8f1d912b92ca
nobody <nobody@zotlabs.com>
2021-04-10 23:39:04 -0700

    cleanup

---
+++ Zotlabs/Update/_1248.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Zotlabs\Update;
+
+class _1248 {
+
+	function run() {
+	
+	    q("START TRANSACTION");
+
+		if(ACTIVE_DBTYPE == DBTYPE_POSTGRES) {
+			$r1 = q("ALTER TABLE atoken ADD atoken_guid NOT NULL DEFAULT '' ");
+ 			$r2 = q("create index \"atoken_guid\" on xchan (\"atoken_guid\")");
+
+			$r = ($r1 && $r2);
+		}
+		else {
+			$r = q("ALTER TABLE `atoken` ADD `atoken_guid` char(191) NOT NULL DEFAULT '' , 
+				ADD INDEX `atoken_guid` (`atoken_guid`)");
+		}
+
+		if($r) {
+			q("COMMIT");
+			return UPDATE_SUCCESS;
+		}
+
+		q("ROLLBACK");
+		return UPDATE_FAILED;
+
+	}
+
+	function verify() {
+
+		$columns = db_columns('atoken');
+
+		if(in_array('atoken_guid',$columns)) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
