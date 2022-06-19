4760766eb8fe8256a93be94de336621a63517de9
Mike Macgirvin <mike@macgirvin.com>
2022-06-19 14:04:20 -0700

    restrict addon visibility

--- Code/Lib/Libzot.php
+++ Code/Lib/Libzot.php
@@ -3277,17 +3277,7 @@ class Libzot
 
         $ret['site']['admin'] = get_config('system', 'admin_email');
 
-        $visible_plugins = [];
-
-        $r = q("select * from addon where hidden = 0");
-        if ($r) {
-            foreach ($r as $rr) {
-                $visible_plugins[] = $rr['aname'];
-            }
-        }
-
         $ret['site']['about'] = bbcode(get_config('system', 'siteinfo'), ['export' => true]);
-        $ret['site']['plugins'] = $visible_plugins;
         $ret['site']['sitehash'] = get_config('system', 'location_hash');
         $ret['site']['sellpage'] = get_config('system', 'sellpage');
         $ret['site']['location'] = get_config('system', 'site_location');
