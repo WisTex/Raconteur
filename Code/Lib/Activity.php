f63eca280d362ed9a680e6ac014b9b6ee3686585
Mike Macgirvin <mike@macgirvin.com>
2022-08-04 07:23:36 +1000

    FEP-e232 - implement quote url as tags in addition to the other dozen ways of doing it.

--- Code/Lib/Activity.php
+++ Code/Lib/Activity.php
@@ -439,16 +439,10 @@ class Activity
                     $t['type'] = 'Hashtag';
                 }
 
-                // Handle quoted posts as taxonomy for the special snowflakes that can't figure out how to use attachments.
-                // For links, look for mediaType containing 'activity' OR link relations containing either 'via' or 'cite-as'
-                if ($t['type'] === 'Note' && isset($t['id'])) {
-                    $ret[] = ['ttype' => TERM_QUOTED, 'url' => t['id'], 'term' => 'quoted_post'];
-                    continue;
-                }
-                elseif ($t['type'] === 'Link' && isset($t['href'])
-                        && ((isset($t['mediaType']) && strpos($t['mediaType'], 'activity') !== false)
-                        || (isset($t['rel']) && (attribute_contains($t['rel'],'via') || attribute_contains($t['rel'],'cite-as'))))) {
-                    $ret[] = ['ttype' => TERM_QUOTED, 'url' => t['href'], 'term' => 'quoted_post'];
+                if ($t['type'] === 'Link' && isset($t['href']) && isset($t['mediaType']) && strpos($t['mediaType'], 'activity') !== false) {
+                    $entry = ['ttype' => TERM_QUOTED, 'url' => $t['href']];
+                    $entry['term'] = ((!empty($t['name'])) ? escape_tags($t['name']) : t('Quoted post'));
+                    $ret[] = $entry;
                     continue;
                 }
 
