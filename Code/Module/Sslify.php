954e7adc8227c4dd0d0ada979291078d1e98e1aa
Mike Macgirvin <mike@macgirvin.com>
2022-08-29 22:31:40 -0700

    Sslify: URL cannot be NULL

--- Code/Module/Sslify.php
+++ Code/Module/Sslify.php
@@ -10,20 +10,23 @@ class Sslify extends Controller
 
     public function init()
     {
-        $x = Url::get($_REQUEST['url']);
-        if ($x['success']) {
-            $h = explode("\n", $x['header']);
-            foreach ($h as $l) {
-                list($k, $v) = array_map("trim", explode(":", trim($l), 2));
-                $hdrs[strtolower($k)] = $v;
-            }
-            if (array_key_exists('content-type', $hdrs)) {
-                $type = $hdrs['content-type'];
-                header('Content-Type: ' . $type);
-            }
+        $url = $_REQUEST['url'];
+        if ($url) {
+            $x = Url::get($url);
+            if ($x['success']) {
+                $h = explode("\n", $x['header']);
+                foreach ($h as $l) {
+                    list($k, $v) = array_map("trim", explode(":", trim($l), 2));
+                    $hdrs[strtolower($k)] = $v;
+                }
+                if (array_key_exists('content-type', $hdrs)) {
+                    $type = $hdrs['content-type'];
+                    header('Content-Type: ' . $type);
+                }
 
-            echo $x['body'];
-            killme();
+                echo $x['body'];
+                killme();
+            }
         }
         killme();
     }
