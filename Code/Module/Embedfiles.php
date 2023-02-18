5a2659461ec3d4eea630db170c0f05a7950c2dd4
Ray <raypeaslee@gmail.com>
2023-02-18 14:00:05 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -35,7 +35,7 @@ class Embedfiles extends Controller
 
         $observer = App::get_observer();
         $result = attach_list_files($channel_id, $observer);
-        json_return_and_die(['status' => true, 'content' => $channel_id]);
+        json_return_and_die(['status' => true, 'content' => $result]);
 
         /* end add new */ 
 
