af80edbf0d4892d52a0815706fad736a7a9e1d7d
Ray <raypeaslee@gmail.com>
2023-02-18 15:54:45 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -31,7 +31,7 @@ class Embedfiles extends Controller
 
         /* start add new */
 
-        
+        $channel_id = App::$profile['profile_uid'];
         $observer = App::get_observer();
         $result = attach_list_files($channel_id, $observer);
         json_return_and_die(['status' => true, 'content' => $result]);
