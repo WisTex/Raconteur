455a65783d986e2eb3663253ddaae3b22c390b09
Ray <raypeaslee@gmail.com>
2023-02-20 17:04:01 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -27,8 +27,8 @@ class Embedfiles extends Controller
         $orderby = 'folder asc';
         
         $results = attach_list_files($channel_id, $observer, $hash = '', $filename = '', $filetype = '', $orderby, $start = 0, $entries = 0, $since = '', $until = '');
-        $results = sort_embed_files($results);
-        json_return_and_die(['status' => true, 'content' => $results]);
+    /*    $results = sort_embed_files($results);    */
+        json_return_and_die(['status' => true, 'content' => is_array($results)]);
 
     }
 
@@ -37,7 +37,7 @@ class Embedfiles extends Controller
         {
             $sorted = new stdClass();
             for($i = 0; $i < count($results); ++$i) {
-                if($results[$i]->folder !== ''){
+                if($results[$i]->folder == ''){
                     $sorted[$i] = $results[$i];
 
                 }
