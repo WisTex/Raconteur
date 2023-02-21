29a97bad497306b5e705bdd88758505271cf5da0
Ray <raypeaslee@gmail.com>
2023-02-21 09:39:19 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -54,7 +54,7 @@ class Embedfiles extends Controller
                         if($result['folder'] === $parent['hash']) {
                     
                         /*  $parent[$sf] = $result; */
-                        $parent['subfolder'][$sf] = $result;
+                        $parent->subfolder->$sf = $result;
 
                                             
                         }
