697b81500c1d6b79fa47f4ddd5a181f5a382f40a
Ray <raypeaslee@gmail.com>
2023-02-23 12:36:55 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -29,10 +29,11 @@ class Embedfiles extends Controller
         $results = $results['results'];
         $count = count($results);
         //$sorted = $this->get_embed_folders($results);
-        $sorted = $this->get_embed_top_folders($results);
+        //$sorted = $this->get_embed_top_folders($results);
         //$sorted = $this->get_embed_sub_folders($results);
         //$sorted = $this->new_test($results);
-        json_return_and_die(['success' => $success, 'content' => $sorted]);
+        //json_return_and_die(['success' => $success, 'content' => $sorted]);
+        json_return_and_die(['success' => $success, 'content' => $results]);
 
     }
 
@@ -66,13 +67,6 @@ class Embedfiles extends Controller
         public function get_embed_top_folders($results)
         {   
 
-            $sorted = array_filter($results[0], function($v, $k) {
-                return $k == 'is_dir' && $v == '';
-            }, ARRAY_FILTER_USE_BOTH);
-
-            return $sorted;
-
-            /*
             $sorted = array();
             $i = 0;
             foreach ($results as $result) {
@@ -82,7 +76,6 @@ class Embedfiles extends Controller
                 }
             }
             return $sorted;
-            */
         }
 
         /* end get_embed_top_folders */
