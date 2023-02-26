3c95b4acf02b4515f82c49f246c295e6e1809d45
Ray <raypeaslee@gmail.com>
2023-02-26 09:39:07 -0500

    Update Embedfiles.php
    change order by

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -23,7 +23,8 @@ class Embedfiles extends Controller
         $channel_id = $channel['channel_id'];
         $channel_address = $channel['channel_address'];
         $observer = get_observer_hash();
-        $orderby = 'is_dir desc';
+        //$orderby = 'is_dir desc';
+        $orderby = 'filename asc';
         
         $results = attach_list_files($channel_id, $observer, $hash = '', $filename = '', $filetype = '', $orderby, $start = 0, $entries = 0, $since = '', $until = '');
         $success = $results['success'];
