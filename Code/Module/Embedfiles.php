260bbd5808c7e839d6f6fdf3be41de31eb1bc00d
Ray <raypeaslee@gmail.com>
2023-03-11 12:53:39 -0500

    Update Embedfiles.php

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -32,7 +32,7 @@ class Embedfiles extends Controller
             $resource_id = $data->hash;
 
             $arr = $data->uid;
-            var_dump(json_decode($json));
+            var_dump($json);
 	print("<pre>".print_r($arr,true)."</pre>");
     exit;
             
