243f6da301ebff9dd2266c1f8cdd79f016240ff0
Ray <raypeaslee@gmail.com>
2023-03-13 08:52:39 -0400

    Update Embedfiles.php
    change sharelink to message in returned json

--- Code/Module/Embedfiles.php
+++ Code/Module/Embedfiles.php
@@ -29,7 +29,7 @@ class Embedfiles extends Controller
 
             $x = self::sharelink($resource_id, $channel_id);
             if ($x) {
-                json_return_and_die(['status' => true, 'sharelink' => $x, 'resource_id' => $resource_id]);
+                json_return_and_die(['status' => true, 'message' => $x, 'resource_id' => $resource_id]);
             }
             json_return_and_die(['errormsg' => 'Error retrieving resource ' . $resource_id, 'status' => false]);
         }
